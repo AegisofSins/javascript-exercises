533950c044759ee60550da952bbd707a982badf3
AegisofSins <113641383+AegisofSins@users.noreply.github.com>
2023-01-04 13:46:07 -0800

    Done 04_removeFromArray.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
-const removeFromArray = function() {
-
-};
+let removeFromArray = function(...args) {
+    const myArr = args[0];
+    return myArr.filter((val) => !args.includes(val))
+    };
 
 // Do not edit below this line
 module.exports = removeFromArray;
