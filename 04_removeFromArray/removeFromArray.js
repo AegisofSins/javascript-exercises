6259bf25598fea2d8080a09ab6ed627a10110214
AegisofSins <113641383+AegisofSins@users.noreply.github.com>
2023-01-04 15:43:38 -0800

    Done 05_sumAll.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
 let removeFromArray = function(...args) {
     const myArr = args[0];
-    return myArr.filter((val) => !args.includes(val))
+    console.log(myArr);
+    return myArr.filter(item => !args.includes(item))
     };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
+let sumAll = function(val1, val2) {
+    let sumAdded =0;
 
-};
+    if ((!Number.isInteger(val1)) || (!Number.isInteger(val2))) return 'ERROR';
+    if ((val1 < 0 || val2 < 0)) return 'ERROR';
+    if(val1 >= val2){
+        [val1,val2]=[val2,val1]
+    };
+   
+    for (let i = val1; i <= val2; i++){
+                sumAdded += i;
+        }
+        return sumAdded;
+    };
 
 // Do not edit below this line
 module.exports = sumAll;
